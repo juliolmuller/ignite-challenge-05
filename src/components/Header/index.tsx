5838e90548d79b10161d831827f9632d7fcf1ee6
Julio Muller <juliodolago@gmail.com>
2022-06-07 12:39:11 -0300

    feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import classes from './header.module.scss';
+
+interface HeaderProps {
+  className?: string;
+}
+
+export default function Header({ className = '' }: HeaderProps): JSX.Element {
+  return (
+    <header className={`${classes.wrapper} ${className}`}>
+      <Link href="/">
+        <a>
+          <img src="/img/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
