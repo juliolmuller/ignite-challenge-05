6035a41900be3c780deddbb2b271dfba0baf72c2
Julio Muller <juliodolago@gmail.com>
2022-06-07 12:38:57 -0300

    style: define global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,26 @@
-import Document from 'next/document';
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
