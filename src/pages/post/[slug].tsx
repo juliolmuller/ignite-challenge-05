4d62ec4db9bd46ee888d050804906b2a825c596e
Julio Muller <juliodolago@gmail.com>
2022-06-08 01:48:21 -0300

    feat: create post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,16 @@
+/* eslint-disable react/no-array-index-key */
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { Fragment, useMemo } from 'react';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
+import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
-import styles from './post.module.scss';
+import classes from './post.module.scss';
 
 interface Post {
   first_publication_date: string | null;
@@ -11,6 +18,7 @@ interface Post {
     title: string;
     banner: {
       url: string;
+      alt?: string;
     };
     author: string;
     content: {
@@ -23,23 +31,98 @@ interface Post {
 }
 
 interface PostProps {
-  post: Post;
+  post?: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  const { isFallback } = useRouter();
+  const readingTime = useMemo(() => {
+    if (!post) return 0;
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+    const wordsPerMinute = 200;
+    const wordsCount = post.data.content.reduce((words, { heading, body }) => {
+      const headingWords = heading.split(' ').length;
+      const bodyWords = body.reduce((acc, { text }) => {
+        return acc + text.split(' ').length;
+      }, 0);
 
-//   // TODO
-// };
+      return words + headingWords + bodyWords;
+    }, 0);
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+    return Math.ceil(wordsCount / wordsPerMinute);
+  }, [post]);
 
-//   // TODO
-// };
+  return (
+    <>
+      <Head>
+        <title>{post?.data.title} | spacetraveling</title>
+      </Head>
+
+      <div className={classes.wrapper}>
+        <Header />
+
+        {isFallback ? (
+          <main className={classes.fallback}>Carregando...</main>
+        ) : (
+          <article>
+            <img src={post.data.banner.url} alt={post.data.banner.alt} />
+
+            <header>
+              <h1>{post.data.title}</h1>
+              <small>
+                <p>
+                  <FiCalendar size={20} />
+                  {format(new Date(post.first_publication_date), 'd MMM yyyy', {
+                    locale: ptBR,
+                  })}
+                </p>
+                <p>
+                  <FiUser size={20} />
+                  {post.data.author}
+                </p>
+                <p>
+                  <FiClock size={20} />
+                  {readingTime} min
+                </p>
+              </small>
+            </header>
+
+            <main>
+              {post.data.content.map(({ heading, body }, index) => (
+                <Fragment key={index}>
+                  <h2>{heading}</h2>
+                  {body.map(({ text }, idx) => (
+                    <p key={idx}>{text}</p>
+                  ))}
+                </Fragment>
+              ))}
+            </main>
+          </article>
+        )}
+      </div>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('post', {
+    pageSize: 10,
+  });
+
+  return {
+    fallback: true,
+    paths: postsResponse.results.map(({ uid }) => ({ params: { slug: uid } })),
+  };
+};
+
+export const getStaticProps: GetStaticProps<PostProps> = async ({ params }) => {
+  const postSlug = String(params?.slug);
+  const prismic = getPrismicClient({});
+  const post: any = await prismic.getByUID('post', postSlug);
+
+  return {
+    props: { post },
+    revalidate: 60 * 60 * 2, // 2 hours
+  };
+};
